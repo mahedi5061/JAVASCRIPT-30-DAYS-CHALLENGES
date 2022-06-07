9a6638af302bb727233397289b75705180941f07
mahedi5061 <mahedi5061@gmail.com>
2022-06-07 16:53:10 +0600

    javascript challenges

---
+++ DRUM KIT/app.js
@@ -0,0 +1,18 @@
+window.addEventListener("keydown", function (e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+
+  audio.currentTime = 0;
+  audio.play();
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  key.classList.add("playing");
+});
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return; //skip it, if it's not a transform
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
